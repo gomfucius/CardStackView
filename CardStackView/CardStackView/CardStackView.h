a6aff19cb762627161192afd7ff3a2e86b478831
Genki Mine <gomfucius@gmail.com>
2018-11-22 11:43:34 -0800

    Add Carthage support

---
+++ CardStackView/CardStackView/CardStackView.h
@@ -0,0 +1,19 @@
+//
+//  CardStackView.h
+//  CardStackView
+//
+//  Created by Genki Mine on 11/22/18.
+//  Copyright © 2018 Genki Mine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CardStackView.
+FOUNDATION_EXPORT double CardStackViewVersionNumber;
+
+//! Project version string for CardStackView.
+FOUNDATION_EXPORT const unsigned char CardStackViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardStackView/PublicHeader.h>
+
+
